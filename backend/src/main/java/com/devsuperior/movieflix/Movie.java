2af0515c864353eb050ceb93634b08fa86f7bc2d
westorres9 <wester.torres9@gmail.com>
2022-07-04 16:34:32 -0300

    Movie

--- backend/src/main/java/com/devsuperior/movieflix/Movie.java
+++
@@ -1,5 +0,0 @@
-package com.devsuperior.movieflix;
-
-public class Movie {
-
-}
--- backend/src/main/java/com/devsuperior/movieflix/entities/Genre.java
+++ backend/src/main/java/com/devsuperior/movieflix/entities/Genre.java
@@ -1,9 +1,18 @@
 package com.devsuperior.movieflix.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.OneToMany;
+
 public class Genre {
 	
 	private Long id;
 	private String name;
+	
+	@OneToMany(mappedBy = "genre")
+	private List<Movie> movies = new ArrayList<>();
 	
 	public Genre() {
 	}
@@ -27,6 +36,27 @@ public class Genre {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public List<Movie> getMovies() {
+		return movies;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Genre other = (Genre) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
