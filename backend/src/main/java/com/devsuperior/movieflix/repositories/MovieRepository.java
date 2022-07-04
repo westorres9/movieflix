5009e768b76e0b5398672c0f55712f104d05da2b
westorres9 <wester.torres9@gmail.com>
2022-07-04 17:11:03 -0300

    User,Role,Review repositories

--- backend/src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
+++ backend/src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -1,16 +1,12 @@
 package com.devsuperior.movieflix.repositories;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-import com.devsuperior.movieflix.entities.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.devsuperior.movieflix.entities.Movie;
 @Repository
-public interface MovieRepository extends JpaRepository<Genre,Long>{
+public interface MovieRepository extends JpaRepository<Movie,Long>{
 	
-	@Query("SELECT obj FROM Movie WHERE :genre IS NULL OR oj.genre = :genre")
-	Page<Movie> find(Genre genre ,Pageable pageable);
+	//@Query("SELECT obj FROM Movie WHERE :genre IS NULL OR oj.genre = :genre")
+	//Page<Movie> find(Genre genre ,Pageable pageable);
 }
